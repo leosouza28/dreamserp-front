236dccdbf947b9c4794e78ad77477a4e1d33e3cb
Joliverto <joaonetow9@gmail.com>
2026-03-31 10:08:12 -0300

    Adição de filtro atualizada

--- src/app/services/endpoints.service.ts
+++ src/app/services/endpoints.service.ts
@@ -104,17 +104,21 @@ export class EndpointsService extends ApiService {
         return this.post('/v1/admin/pessoas', data);
     }
     // CRM
-    getCrms({ perpage, page, q, ...params }: any) {
+    getCrms({ perpage, page, q, tags, ...params }: any) {
         let urlParams = new URLSearchParams();
         if (perpage && page) {
             urlParams.append('perpage', perpage);
             urlParams.append('page', page);
         }
         if (q) urlParams.append('q', q);
+        if (tags) urlParams.append('tags', tags);
         for (const key in params) {
             if (params[key]) urlParams.append(key, params[key]);
         }
         return this.get('/v1/admin/crm' + (urlParams.toString() ? `?${urlParams.toString()}` : ''));
+    }
+    getCrmTagKeys() {
+        return this.get('/v1/admin/crm/tags');
     }
     getCrmById(id: string) {
         return this.get('/v1/admin/crm/' + id);
